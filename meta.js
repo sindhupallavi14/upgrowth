16aa56a9121491acc48c1b0a381cbc9122b10829
sindhupallavi14 <sindhuramanadula14@gmail.com>
2025-06-10 16:50:12 +0530

    first commit

---
+++ meta.js
@@ -0,0 +1,39 @@
+const metadata={
+    meta: {
+  title: "Compare Blinkit, Zepto, Instamart Prices | CompareKaro",
+  description: "Compare prices and delivery times in real-time across Blinkit, Zepto, Instamart, and more with CompareKaro. Save time and money on groceries and essentials.",
+  keywords: "Blinkit vs Zepto vs Instamart, quick commerce price comparison, grocery delivery price check, compare instant delivery apps",
+  structuredData: {
+    "@context": "http://schema.org",
+    "@type": "BlogPage",
+    name: "Compare Quick Commerce Prices in Real-Time",
+    url: "https://comparekaro.app/",
+    description: "Compare prices and delivery times in real-time across Blinkit, Zepto, Instamart, and more with CompareKaro. Save time and money on groceries and essentials.",
+  },
+},
+meta: {
+  title: "Compare Quick Commerce Insights and Tips",
+  description: "Explore the CompareKaro blog for the latest updates, tips, and insights on comparing prices across top quick commerce platforms like Blinkit, Zepto, and Instamart.",
+  keywords: "CompareKaro blog, quick commerce insights, Blinkit vs Zepto, Instamart tips, price comparison, fast delivery apps",
+  structuredData: {
+    "@context": "http://schema.org",
+    "@type": "BlogPage",
+    name: "CompareKaro Blog",
+    url: "https://comparekaro.app/blogs",
+    description: "Explore the CompareKaro blog for the latest updates, tips, and insights on comparing prices across top quick commerce platforms like Blinkit, Zepto, and Instamart.",
+  },
+},
+meta: {
+  title: "Privacy Policy",
+  description: "Learn how CompareKaro safeguards your personal data and ensures privacy. Read our detailed privacy policy to understand how we protect your information.",
+ keywords: "privacy policy, data protection, CompareKaro privacy, personal data security, user information policy",
+  structuredData: {
+    "@context": "http://schema.org",
+    "@type": "BlogPage",
+    name: "CompareKaro Blog",
+    url: "https://comparekaro.app/privacy-policy",
+    description: "Learn how CompareKaro safeguards your personal data and ensures privacy. Read our detailed privacy policy to understand how we protect your information.",
+  },
+},
+
+}
